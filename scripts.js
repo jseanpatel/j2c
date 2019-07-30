49380cb6e9700db5850f007bbce147bc2568ad1d
alexisqueenn <alexism.queen19@gmail.com>
2019-07-30 10:35:30 -0700

    Basic functions.

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+
+function storeGrade(user) {
+  var elem = document.getElementById()
+  print(user)
+}
+
+function storeQuestion(needs) {
+  var elem = document.getElementById()
+  calendar[].add(needs)
+}
+
+function storeState(state) {
+  var elem = document.getElementById()
+calendar[].add(stateInfo)
+}
