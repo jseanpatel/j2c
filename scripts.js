78885a9d272d8bcb54933d47501be404b36fbfaa
alexisqueenn <alexism.queen19@gmail.com>
2019-07-30 18:07:30 -0700

    changed navigation bars

--- scripts.js
+++ scripts.js
@@ -92,8 +92,43 @@ function appendPre(message) {
 /**
  * Print the summary and start datetime/date of the next ten events in
  * the authorized user's calendar. If no events are found an
- * appropriate message is printed.
- */
+ * appropriate message is printed. */
+
+ <a role="button" class="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
+  <span aria-hidden="true"></span>
+  <span aria-hidden="true"></span>
+  <span aria-hidden="true"></span>
+</a>
+
+// document.addEventListener('DOMContentLoaded', () => {
+// 
+//   // Get all "navbar-burger" elements
+//   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+//
+//   // Check if there are any navbar burgers
+//   if ($navbarBurgers.length > 0) {
+//
+//     // Add a click event on each of them
+//     $navbarBurgers.forEach( el => {
+//       el.addEventListener('click', () => {
+//
+//         // Get the target from the "data-target" attribute
+//         const target = el.dataset.target;
+//         const $target = document.getElementById(target);
+//
+//         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+//         el.classList.toggle('is-active');
+//         $target.classList.toggle('is-active');
+//
+//       });
+//     });
+//   }
+
+});
+<div class="navbar-menu" id="navMenu">
+  <!-- navbar-start, navbar-end... -->
+</div>
+
 function listUpcomingEvents() {
   gapi.client.calendar.events.list({
     'calendarId': 'primary',
