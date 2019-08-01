0da9e282623d870b82fb1ec9a98127a04c1f6ecc
jseanpatel <jseanpatel@gmail.com>
2019-08-01 12:10:42 -0700

    minor calendar changes

--- scripts.js
+++ scripts.js
@@ -103,44 +103,29 @@
    });
  }
 
- function makeTestingEvent() {
-  var event = {
-    'summary': 'Google I/O 2015',
-    'location': '800 Howard St., San Francisco, CA 94103',
-    'description': 'A chance to hear more about Google\'s developer products.',
-    'start': {
-      'dateTime': '2015-05-28T09:00:00-07:00',
-      'timeZone': 'America/Los_Angeles'
-    },
-    'end': {
-      'dateTime': '2015-05-28T17:00:00-07:00',
-      'timeZone': 'America/Los_Angeles'
-    },
-    'recurrence': [
-      'RRULE:FREQ=DAILY;COUNT=2'
-    ],
-    'attendees': [
-      {'email': 'lpage@example.com'},
-      {'email': 'sbrin@example.com'}
-    ],
-    'reminders': {
-      'useDefault': false,
-      'overrides': [
-        {'method': 'email', 'minutes': 24 * 60},
-        {'method': 'popup', 'minutes': 10}
-      ]
-    }
-  };
+//  function makeTestingEvent() {
+//   var event = {
+   
+//     "start": {
+//       "dateTime": "2015-05-28T09:00:00-07:00",
+//       "timeZone": "America/Los_Angeles"
+//     },
+//     "end": {
+//       "dateTime": "2015-05-28T17:00:00-07:00",
+//       "timeZone": "America/Los_Angeles"
+//     }
   
-  var request = gapi.client.calendar.events.insert({
-    'calendarId': 'primary',
-    'resource': event
-  });
+//   };
   
-  request.execute(function(event) {
-    appendPre('Event created: ' + event.htmlLink);
-  });
- }
+//   var request = gapi.client.calendar.events.insert({
+//     "calendarId": "primary",
+//     "resource": event
+//   });
+  
+//   request.execute(function(event) {
+//     appendPre("Event created: " + event.htmlLink);
+//   });
+//  }
 
 // index.html
 
